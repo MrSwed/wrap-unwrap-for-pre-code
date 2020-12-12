5575f9e1190b8f77f37615600878e646b80e6823
Sergey Davydov <webmaster@sdcollection.com>
2020-12-12 06:53:53 +0000

    fix code height to scroll it in place

--- logic.js
+++ logic.js
@@ -1,10 +1,11 @@
 // https://stackoverflow.com/a/57341170/5447232
-var ws = function(i,e) { 
-  $("pre code").filter(function(){
-   var _t = $(this).parent();
-    return !_t.has("input[switch").length 
-      && _t.get(0).scrollWidth > _t.innerWidth();
-  }).before("<input type='checkbox' checked switch title='Wrap/Unwrap' />");
+var ws = function(i, e) {
+  $("pre code").filter(function() {
+    var _c=$(this),_p = _c.parent();
+    return !_p.has("input[switch").length 
+      && _c.get(0).scrollWidth > _c.innerWidth() 
+      && _c.height(_c.outerHeight());
+  }).before("<input type='checkbox' switch title='Wrap/Unwrap' />");
 }
 $("body").on("change", ws).change();
 $(window).bind("scroll", ws);
