e6be677b2e968f1bc1483178d75c989360d83838
Sergey Davydov <dev@sdcollection.com>
2021-07-24 07:57:57 +0000

    fix error

--- logic.js
+++ logic.js
@@ -2,7 +2,7 @@
 var ws = function(i, e) {
   $("pre code").filter(function() {
     var _c=$(this),_p = _c.parent();
-    return !_p.has("input[switch").length 
+    return !_p.has("input[switch]").length 
       && _c.get(0).scrollWidth > _c.innerWidth() 
       && _c.height(_c.outerHeight());
   }).before("<input type='checkbox' switch title='Wrap/Unwrap' />");
